72c673b85a7076c00db57a234d85d0caad851a44
dereksilva64 <derekvasconcelos64@gmail.com>
2024-05-24 15:47:52 -0300

    Ajustes

--- admin/includes/connection.php
+++ admin/includes/connection.php
@@ -1,9 +1,9 @@
 <?php 
 
-    const DB_HOST = 'localhost';
-    const DB_USER = 'root';
-    const DB_PASS = '';
-    CONST DB_NAME = 'ipbeach';
+    const DB_HOST = 'files.000webhost.com';
+    const DB_USER = 'id22209548_ipbeachsports';
+    const DB_PASS = '@Dextreme64';
+    CONST DB_NAME = 'id22209548_admin';
     
     $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
